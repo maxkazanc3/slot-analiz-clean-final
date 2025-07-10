43380b855650dbd851ebb80efc2abaa9ba800174
maxkazanc3 <maxkazanc3@gmail.com>
2025-07-10 23:40:25 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>✅ Slot Analiz Paneli</h1>
+      <p>Bu panel başarıyla çalışıyor!</p>
+    </div>
+  );
+}
